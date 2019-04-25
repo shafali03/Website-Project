7f280f9c13a148183410a50438d52dce836ffd6f
shafali03 <shaf03sm@gmail.com>
2019-04-25 19:43:56 +0100

    preloader working

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+// hide preloader
+// all the image script links have finished loading
+
+// window event list
+
+window.addEventListener('load', function () {
+    document.querySelector('.preloader').style.display = "none";
+})
