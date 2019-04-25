35f967e998d7b71707b8b04069609424f598ae8a
shafali03 <shaf03sm@gmail.com>
2019-04-25 21:03:25 +0100

    update

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@
 // hide preloader
 // all the image script links have finished loading
 
-// window event list
 
-window.addEventListener('load', function () {
-    document.querySelector('.preloader').style.display = "none";
-})
+//refactor
+function eventListeners() {
+    // window event list
+    window.addEventListener('load', function () {
+        document.querySelector('.preloader').style.display = "none";
+    })
+    // nav btn
+    document.querySelector('.navBtn').addEventListener('click', function () {
+        document.querySelector('.nav').classList.toggle('nav--show');
+    })
+}
+
+
