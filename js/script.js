599cd6f84ab6c26a11aa59046fb6562e100a6067
shafali03 <shaf03sm@gmail.com>
2019-04-26 21:58:08 +0100

    update

--- js/script.js
+++ js/script.js
@@ -7,26 +7,38 @@ eventListeners();
 function eventListeners() {
     const ui = new UI()
 
+    //preloader
     window.addEventListener('load', function () {
         ui.hidePreloader();
     })
 
+    // nav btn
     document.querySelector('.navBtn').addEventListener('click', function () {
         ui.showNav()
     })
+    // control the video
+    document.querySelector('.video__switch').addEventListener('click', function () {
+        ui.videoControls()
+    })
+
 }
 
 
-//constructor
+//constructor function
 function UI() {
 
 }
 
+// hide reloader
 UI.prototype.hidePreloader = function () {
     document.querySelector('.preloader').style.display = "none";
 }
 
+// show nav
 UI.prototype.showNav = function () {
     document.querySelector('.nav').classList.toggle('nav--show');
 }
 
+UI.prototype.videoControls = function () {
+    document.querySelector(.)
+}
