f55fee4912460f33c810a0b79b155ea2e3bc8e36
shafali03 <shaf03sm@gmail.com>
2019-04-27 14:41:40 +0100

    update work section html

--- js/script.js
+++ js/script.js
@@ -20,11 +20,36 @@ function eventListeners() {
     document.querySelector('.video__switch').addEventListener('click', function () {
         ui.videoControls()
     })
+    //submit the form
+    document.querySelector('.drink-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const name = document.querySelector('.input-name').value;
+        const lastName = document.querySelector('.input-lastname').value;
+        const email = document.querySelector('.input-email').value;
 
+        let value = ui.checkEmpty(name, lastName, email)
+
+        if (value) {
+            ui.showFeedBack('Thanks you', 'success');
+            ui.clearFields();
+        }
+        else {
+            ui.showFeedBack('some form value are empty', 'error')
+        }
+    })
 }
 
 
-//constructor function
+
+
+
+
+
+
+
+
+
+//constructor function ============================================
 function UI() {
 
 }
@@ -39,6 +64,61 @@ UI.prototype.showNav = function () {
     document.querySelector('.nav').classList.toggle('nav--show');
 }
 
+//control video play or pause
 UI.prototype.videoControls = function () {
-    document.querySelector(.)
+    let btn = document.querySelector('.video__switch-btn');
+    if (!btn.classList.contains('btnSlide')) {
+        btn.classList.add('btnSlide')
+        document.querySelector('.video__item').pause()
+    }
+    else {
+        btn.classList.remove('btnSlide')
+        document.querySelector('.video__item').play()
+    }
+}
+
+// check for empty value
+
+UI.prototype.checkEmpty = function (name, lastname, email) {
+    let result;
+    if (name === '' || lastname === '' || email === '') {
+        result = false;
+    }
+    else {
+        result = true;
+    }
+    return result;
+}
+
+UI.prototype.showFeedBack = function (text, type) {
+    const feedback = document.querySelector('.drink-form__feedback');
+
+    if (type === 'success') {
+        feedback.classList.add('success');
+        feedback.innerText = text;
+        this.removeAlert('success');
+
+    }
+    else if (type === 'error') {
+        feedback.classList.add('error');
+        feedback.innerText = text;
+        this.removeAlert('error');
+    }
+}
+
+// remove alert
+
+UI.prototype.removeAlert = function (type) {
+    setTimeout(function () {
+        document.querySelector('.drink-form__feedback').classList.remove(type)
+    }, 3000)
+}
+
+// clear fields
+
+UI.prototype.clearFields = function () {
+    document.querySelector('.input-name').value = '';
+    document.querySelector('.input-lastname').value = '';
+    document.querySelector('.input-email').value = '';
+
 }
